51aa7d064111c3d292d274edf6f09749ef3a6ad0
Daniela V <liliangarcia@Lilians-MacBook-Air.local>
2023-05-27 13:06:32 -0400

    added animation to progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -8,8 +8,10 @@ function ProgressBar({ current, max }: ProgressBarProps) {
   const valueAsPercentage = `${value}%`
 
   return (
+    <div className="animate-pulse flex space-x-4 ">
     <div className="absolute inset-x-0 top-0 h-3">
-      <div className="h-full bg-primary-600" style={{ width: valueAsPercentage }} />
+      <div className="h-full bg-primary-600 progress-bar" style={{ width: valueAsPercentage }} />
+    </div>
     </div>
   )
 }
